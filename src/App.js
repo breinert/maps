105c11aca422ff60f1d632606336c93ae71a6a0c
Brian Reinert <breinert971@gmail.com>
2018-10-17 17:10:52 -0400

    feat: Add foursquare query

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import './App.css'
 import Searchtab from './components/Searchtab'
+import MarkerModal from './components/MarkerModal'
 import axios from 'axios'
 require ('dotenv').config()
 
@@ -12,17 +13,24 @@ class App extends React.Component {
       startPosition: [],
       finishPosition: [],
       venues: [],
-      venueLocation: [],
+      venueLocation: "",
       coffeeLocation: "choose",
     }
     this.handleCoffeeLocation = this.handleCoffeeLocation.bind(this);
     this.handleFindCoffee = this.handleFindCoffee.bind(this);
   }
-
+  // initialize the map
   componentDidMount() {
     this.loadMap()
   }
+  // check for change in venueLocation
+  componentDidUpdate(prevState) {
+    if (prevState.venueLocation !== this.state.venueLocation) {
+      this.getStores()
+    }
+  }
 
+  }
   loadMap = () => {
     loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDUZDt6xP79oqTXaAB6leSmMCYzZkc4Zdo&callback=initMap")
     window.initMap = this.initMap
@@ -73,18 +81,21 @@ class App extends React.Component {
       })
       console.log(event.latLng.lat());
     });
-
-    // const infowindow = new google.maps.InfoWindow();
-
+    const infowindow = new google.maps.InfoWindow();
+    //make markers for stores
     this.state.venues.map(myVenue => {
       const marker = new google.maps.Marker({
         position: {lat: myVenue.venue.location.lat, lng: myVenue.venue.location.lng},
         map: map,
         title: myVenue.venue.name
-
       });
+      const contentString = `${myVenue.venue.name}`;
+      marker.addListener('click', function() {
+        infowindow.setContent(contentString)
+        infowindow.open(map, marker);
+      })
     })
-      // const contentString = `${myStart.title}`;
+
 
   }
 
@@ -94,21 +105,23 @@ class App extends React.Component {
 
   // function to obtain info from foursquare
   getStores = () => {
+    // const location = this.state.venueLocation;
+    console.log(this.state.venueLocation);
     const endPoint = "https://api.foursquare.com/v2/venues/explore?"
     const parameters = {
-      client_id: process.env.REACT_APP_FS_ID,
-      client_secret: process.env.REACT_APP_FS_CS,
-      categoryId: "4bf58dd8d48988d1e0931735",
-      radius: "250",
+      client_id: "JDO1KAGDULQO3ETFJ4EDPEHN203BEDKSD1HB5UUKRDP2R3H2",
+      client_secret: "UWMFNIMGT33V31ZGCVI1GICMRK43DSYBJSNBNJTKC2ECKIHH",
+      query: "coffee",
+      radius: "1000",
       ll: this.state.venueLocation,
-      v: "20181016"
+      v: "201811010"
     }
 
     axios.get(endPoint + new URLSearchParams(parameters))
     .then(response => {
       this.setState({
         venues: response.data.response.groups[0].items
-      }, this.renderMap())
+      })
     })
     .catch(error => {
       console.error("error", error)
@@ -120,31 +133,30 @@ class App extends React.Component {
     this.setState({
       coffeeLocation: e.target.value
     });
-    console.log(`After click: ${e.target.value}`)
+    console.log(`After click: ${this.state.coffeeLocation}`)
   }
 
   handleFindCoffee() {
-    const venueLocation = this.state.venueLocation;
+    // const venueLocation = this.state.venueLocation;
     const startPosition = this.state.startPosition;
     const finishPosition = this.state.finishPosition;
     const coffeeLocation = this.state.coffeeLocation;
 
     if (coffeeLocation === 'rideStart') {
       this.setState({
-        venueLocation: startPosition
-      });
+        venueLocation: startPosition.toString()
+      })
     } else if (coffeeLocation === 'rideFinish') {
-        this.setState({
-          venueLocation: finishPosition
-        });
+      this.setState({
+        venueLocation: finishPosition.toString()
+      })
     } else if (coffeeLocation === 'both') {
       this.setState({
-          venueLocation: [startPosition, finishPosition]
-      });
+        venueLocation: startPosition.toString() + finishPosition.toString()
+      })
     } else {
       alert("Select a location for coffee");
     }
-    this.getStores();
   }
 
   render() {
@@ -166,7 +178,8 @@ class App extends React.Component {
           </div>
         </main>
         <aside>
-          <div id="foursquare"></div>
+          <MarkerModal
+          />
         </aside>
       </div>
     );
